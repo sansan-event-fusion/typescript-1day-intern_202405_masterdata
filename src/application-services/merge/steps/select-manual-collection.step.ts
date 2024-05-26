ab5716d0e73d7ad22bf303aa9a82d2611f9a5e2e
Toshiki <toshiki.fujii@outlook.com>
2024-05-26 17:14:09 +0900

    SelectManualCollectionStep

--- src/application-services/merge/steps/select-manual-collection.step.ts
+++ src/application-services/merge/steps/select-manual-collection.step.ts
@@ -1,10 +1,19 @@
 import { MergeWorkflowStep } from 'src/types/merge-workflow-step';
+import { DataSources } from 'src/value/data-source';
 
 /**
  * 手収集した拠点情報が存在する場合は「SOC単位で」その拠点情報を優先する
  */
 export const SelectManualCollectionStep: MergeWorkflowStep = (data) => {
   // 人力収集データがない場合は, 何もしないで受け取った値をそのまま返す
+  console.log(data);
+  const manual_collection = data.in.attributes.filter(
+    (attribute) => attribute.data_source === DataSources.MANUAL_COLLECTION,
+  );
+
+  if (manual_collection.length === 0) {
+    return data;
+  }
 
   // 人力収集データがある場合は、人力収集データと修正データのみ絞り込む
   const attributes = [];
