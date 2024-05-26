d0a8382a681bbba2bfd5ae0ba74ea8a73f23a51b
bvv-1 <fhinnoigsr@gmail.com>
2024-05-26 17:15:10 +0900

    Implement latest step for integration

--- src/application-services/merge/steps/select-latest.step.ts
+++ src/application-services/merge/steps/select-latest.step.ts
@@ -6,9 +6,19 @@ import { MergeWorkflowStep } from 'src/types/merge-workflow-step';
  */
 export const SelectLatestStep: MergeWorkflowStep = (data) => {
   // 適切な集約の単位にグループを作成する
+  // lodashのgroupByを使う
+  const attributeGroupBySLC = _.groupBy(data.in.attributes, (attribute) => {
+    return attribute.sansan_location_code + attribute.attribute; // SLCと属性を結合した文字列をキーにする
+  });
 
   // 分割したグループ単位で、最新のデータを選択する
-  const attributes = [];
+  const attributes = Object.values(attributeGroupBySLC)
+    .map((group) => {
+      return _.maxBy(group, (attribute) => {
+        return attribute.crawled_at;
+      });
+    })
+    .flat();
 
   return {
     in: {
